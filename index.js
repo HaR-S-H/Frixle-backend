2a1a939c31d9b10c33c1ab70d099415e32cd51b4
HaR-S-H <119717186+HaR-S-H@users.noreply.github.com>
2024-12-23 21:06:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 8080;
 
 // CORS Middleware
 app.use(cors({
-  origin: ['https://inquisitive-pavlova-f0b954.netlify.app'],
+  origin: "*",
   credentials: true
 }));
 
