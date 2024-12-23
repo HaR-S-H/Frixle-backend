f1294a09d30d0fbc8394bcac06b1f4293c07aa77
HaR-S-H <119717186+HaR-S-H@users.noreply.github.com>
2024-12-23 22:41:15 +0530

    Update auth.controllers.js

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -80,7 +80,7 @@ const logInUser = asyncHandler(async (req, res) => {
     const token = existingUser.generateAccessToken();
    const cookieOptions = {
         httpOnly: true,
-       // secure: process.env.NODE_ENV === "production", // Only send cookie over HTTPS in production
+       secure: process.env.NODE_ENV === "production", // Only send cookie over HTTPS in production
         sameSite: 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     };
